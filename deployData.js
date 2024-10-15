69862be2a1c485215929f6b8d23b5f2ad0c9b4ca
Septian Dwi Saputra <114676493+aMimir-droid@users.noreply.github.com>
2024-10-15 17:37:55 +0700

    added another

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Septian Dwi Saputra'; // TODO: replace this
+
+const studentId = 'FS10785928'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
